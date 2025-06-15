706ad277e00acef6158747f52c424c8caaf23498
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2025-06-15 17:58:09 -0500

    started game thread

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // Screen Config
 
@@ -16,10 +16,27 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
+
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // Game loop
+        while(gameThread != null) {
+
+        }
+
+    }
+
 }
