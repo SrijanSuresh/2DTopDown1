029fcd4f686a02d059393dfbf2d317c07d86ea39
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2025-06-15 17:44:03 -0500

    updated panel and window

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class GamePanel extends JPanel {
+
+    // Screen Config
+
+    // 16x16 tile
+    final int orignalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = orignalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+}
